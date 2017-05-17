a4cb0530b4035aa05b1675ee0692e91d5a04a1d1
sergey.garnov <sergeygarnov1991@gmail.com>
2017-05-17 11:40:09 +0300

    wakeup

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 /**
  * Created by admin on 5/14/2017.
  */
+var config = require('./config');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.json({status: "OK"});
+});
+
+app.listen(config.port, function () {
+    console.log('Example app listening on port 3000!');
+});
+
 var telegram = require('./libs/telegram')
 telegram.Telegram();
