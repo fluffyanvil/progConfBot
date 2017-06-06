aebcfd6d80d20ad7e87e8d0e8dc3467547120f57
Sergey <sergeygarnov1991@gmail.com>
2017-06-06 18:21:26 +0300

    cors

--- libs/web.js
+++ libs/web.js
@@ -6,6 +6,13 @@ var cors = require('cors')
 var app = express();
 app.use(express.static('public'));
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 require('./routes')(app);
 
 var server = app.listen(process.env.PORT, function () {
