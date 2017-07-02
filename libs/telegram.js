3d49090cf74ccdb38f28168c4c394605532ac581
Sergey <sergeygarnov1991@gmail.com>
2017-07-03 00:36:05 +0300

    fresh blood

--- libs/telegram.js
+++ libs/telegram.js
@@ -29,7 +29,7 @@ var telegram = function(){
     bot.on('newChatMembers', function(msg){
         console.log(msg);
         var user = msg.new_chat_member;
-        var message = `*Ассалам алейкум, ${ user.first_name } ${ user.last_name }!* \n *Каковы твои язык программирования, зп,* _ориентация_ *?*`;
+        var message = `*Ассалам алейкум, ${ user.first_name } ${ user.last_name }!*\n*Каковы твои язык программирования, зп,* _ориентация_ *?*`;
         // return bot.sendPhoto(msg.chat.id, "https://i.ytimg.com/vi/nCpjBDCauhA/hqdefault.jpg", {caption:'привет'});
         return bot.sendMessage(msg.chat.id, message, {parseMode:'Markdown', replyToMessage:msg.message_id});
     });
