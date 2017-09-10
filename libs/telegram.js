55d017e5aa4dd5c2ac69c3ac444754187a44fb38
Sergey <sergeygarnov1991@gmail.com>
2017-09-10 13:13:46 +0300

    2d tyan

--- libs/telegram.js
+++ libs/telegram.js
@@ -33,7 +33,7 @@ var telegram = function(){
         if (user.id == config.bot_id)
             return;
         var message = '*У нас новый участник!*\n';
-        message = message.concat(`*Ассалам алейкум, ${ user.first_name == null ? '' : user.first_name }${ user.last_name == null ? '' : ' ' + user.last_name }!*\n`);
+        message = message.concat(`*Ohayō gozaimasu, ${ user.first_name == null ? '' : user.first_name }${ user.last_name == null ? '' : ' ' + user.last_name }!*\n`);
         message = message.concat('*Каковы твои возраст, стек технологий, зп,* _ориентация_*?*\n');
         message = message.concat('*Кем видишь себя через 5 лет сидения в этом чате?*');
         mongo.OnUserJoined(msg);
