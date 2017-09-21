15f86e618d8f4a0d23e9120d9780759412ff18fe
Sergey <sergeygarnov1991@gmail.com>
2017-09-21 23:52:58 +0300

    fixed tags for media message, added leftChatMemberEvent

--- libs/telegram.js
+++ libs/telegram.js
@@ -163,7 +163,8 @@ let telegram = function(){
             return;
         let message = '*Нас покинул* ';
         message = message.concat(`[${ user.first_name === null ? '' : user.first_name }${ user.last_name === null ? '' : ' ' + user.last_name }](tg://user?id=${user.id})!\n`);
-        message = message.concat('*Аминь😢*');
+        message = message.concat('*Аминь😢*\n');
+        message = message.concat('#leave\n');
         return bot.sendMessage(msg.chat.id, message, {parseMode:'Markdown'});
     });
 
